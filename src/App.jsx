ddd9797d950805e6b286cecaaa0987258e2b780b
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-01 17:35:38 +0600

    feat: improve tab layout and styling for Products and Cart buttons in App component

--- src/App.jsx
+++ src/App.jsx
@@ -38,33 +38,39 @@ function App() {
       <PremiumTools />
 
       {/* Tabs */}
-      <div className="tabs tabs-box bg-transparent font-bold justify-center mb-9">
+      <div className="flex justify-center">
+        <div className="flex justify-center mb-9 bg-gray-100 rounded-full w-max p-1">
+          {/* Products Button */}
+          <input
+            onClick={() => setActiveTab("Products")}
+            type="radio"
+            name="tabs"
+            className={`tab cursor-pointer w-40 sm:w-44 py-2 sm:py-3 text-center font-semibold transition-all duration-300 ease-in-out
+              border-2 border-gray-300
+              rounded-full
+              ${activeTab === "Products"
+                ? "bg-gradient-to-r from-indigo-600 via-violet-600 to-purple-600 text-white shadow-md border-indigo-600"
+                : "text-gray-900 bg-white hover:bg-gradient-to-r hover:from-indigo-600 hover:via-violet-600 hover:to-purple-600 hover:text-white shadow-sm"
+              }`}
+            aria-label="Products"
+            defaultChecked
+          />
 
-        <input
-          onClick={() => setActiveTab("Products")}
-          type="radio"
-          name="tabs"
-          className={`tab w-40 rounded-full cursor-pointer
-          ${activeTab === "Products"
-              ? "bg-gradient-to-r from-indigo-600 via-violet-600 to-purple-600 text-white"
-              : ""
-            }`}
-          aria-label="Products"
-          defaultChecked
-        />
-
-        <input
-          onClick={() => setActiveTab("Cart")}
-          type="radio"
-          name="tabs"
-          className={`tab w-40 rounded-full cursor-pointer
-          ${activeTab === "Cart"
-              ? "bg-gradient-to-r from-indigo-600 via-violet-600 to-purple-600 text-white"
-              : ""
-            }`}
-          aria-label={`Cart (${cards.length})`}
-        />
-
+          {/* Cart Button */}
+          <input
+            onClick={() => setActiveTab("Cart")}
+            type="radio"
+            name="tabs"
+            className={`tab cursor-pointer w-40 sm:w-44 py-2 sm:py-3 text-center font-semibold transition-all duration-300 ease-in-out
+              border-2 border-gray-300
+              rounded-full
+              ${activeTab === "Cart"
+                ? "bg-gradient-to-r from-indigo-600 via-violet-600 to-purple-600 text-white shadow-md border-indigo-600"
+                : "text-gray-900 bg-white hover:bg-gradient-to-r hover:from-indigo-600 hover:via-violet-600 hover:to-purple-600 hover:text-white shadow-sm"
+              }`}
+            aria-label={`Cart (${cards.length})`}
+          />
+        </div>
       </div>
 
 
