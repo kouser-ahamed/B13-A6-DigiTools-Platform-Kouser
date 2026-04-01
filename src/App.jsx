7a42c11d8894586de59d337fdf16120a944a3a53
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-04-01 07:15:41 +0600

    feat: add ModelLode component and integrate react-toastify for notifications; include model data

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import PremiumTools from './Component/Herosection/PremiumTools'
 import SmallHero from './Component/Herosection/SmallHero'
 import Navbar from './Component/navbar/Navbar'
 import Footer from './Component/Footer/Footer'
+import ModelLode from './Component/AllModelCard/ModelLode'
+
+
+
+const promisedata = async () => {
+  const response = await fetch('/modeldata.json')
+  return response.json()
+
+}
+
+const dataPromise = promisedata()
+console.log(dataPromise)
 
 function App() {
 
@@ -15,7 +27,12 @@ function App() {
     <Banner> </Banner>
     <SmallHero></SmallHero>
     <PremiumTools></PremiumTools>
+
+
+
+    <ModelLode dataPromise={dataPromise}></ModelLode>
     <Footer></Footer>
+
     </>
 
     
---
+++ src/Component/AllModelCard/ModelLode.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ModelLode = ({dataPromise}) => {
+
+    console.log(dataPromise)
+    return (
+        <div>
+          
+            
+        </div>
+    );
+};
+
+export default ModelLode;
